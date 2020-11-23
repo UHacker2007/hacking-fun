0d7f0f8dfa6fb0576e7f965aee36a87bf4c6216a
hackerhijacker <74289017+hackerhijacker@users.noreply.github.com>
2020-11-23 09:32:04 +0530

    Create Hack.py

---
+++ Hack.py
@@ -0,0 +1,19 @@
+print("Hacker2007")
+print("black hat hacker")
+name = 
+input('What is your name?\n')
+print ('Hi, %s.' % name)
+# Python Program - Get String Input from User
+str = input("Enter any string: ")
+print(str)
+# User enters the year
+year = int(input("Enter Year: "))
+# Leap Year Check
+if year % 4 == 0 and year % 100 != 0:
+    print(year, "is a Leap Year")
+elif year % 100 == 0:
+    print(year, "is not a Leap Year")
+elif year % 400 ==0:
+    print(year, "is a Leap Year")
+else:
+    print(year, "is not a Leap Year")
